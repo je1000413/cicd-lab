7306ac7bb7460eddf8a672fe9895298a6e37818f
jessie <je1000413@gmail.com>
2026-06-12 06:24:49 +0000

    Change message to v2

--- src/main/java/com/example/cicddemo/HelloController.java
+++ src/main/java/com/example/cicddemo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "HelloHello CI/CD - v2";
+        return "Hello CI/CD - v2";
     }
 
     @GetMapping("/health")
